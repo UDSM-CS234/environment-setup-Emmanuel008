e00ec1b8781402ac49945b0095b4ed6ae0486981
unknown <emmanueljeremiah008@gmail.com>
2022-05-01 15:55:03 +0200

    initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,14 @@
 package com.company;
 
+
 public class Main {
+
+
+    public static double computePosition(double gravity, double fallingTime, double initialVelocity, double initialPosition){
+        double finalPosition;
+        finalPosition =  0.5 * gravity * fallingTime * fallingTime + initialVelocity * fallingTime + initialPosition;
+        return finalPosition;
+    }
 
     public static void main(String[] args) {
         double gravity = -9.81; // Earth's gravity in m/s^2
@@ -8,7 +16,11 @@ public class Main {
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
+        finalPosition = computePosition(gravity, fallingTime, initialVelocity, initialPosition);
+
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
+
 }
